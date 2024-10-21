8ee5936e825e4925a86d879c373ead346b30df74
Yukesh K <65612359+yukeshk03@users.noreply.github.com>
2024-10-21 18:18:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
-sudo apt-get update
-sudo apt-get install -y build-essential gcc
+#!/bin/bash
+
+# Update pip to the latest version
+pip install --upgrade pip
+
+# Install required Python packages from requirements.txt
+pip install -r requirements.txt
+
+# Download the spaCy model
+python -m spacy download en_core_web_sm
 
